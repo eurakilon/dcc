02555293ae4d6f6011152ca6ee5d2159ebe26046
eurakilon <jean.sordes@yahoo.fr>
2018-04-06 11:04:48 +0200

    Code du projet

---
+++ src/duckcoincoin/Transaction.java
@@ -0,0 +1,20 @@
+package duckcoincoin;
+
+public class Transaction {
+	private int max_value = 1;
+	private String content;
+	
+	/**
+	 * Constructeur
+	 */
+	public Transaction () {
+		this.content = Utility.sha256(Integer.toString(Utility.rdm(100)));
+	}
+	
+	/**
+	 * @return "Transaction : " + contenu de la transaction
+	 */
+	public String toString () {
+		return "Transaction : " + this.content;
+	}
+}
