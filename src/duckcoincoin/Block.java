381f67bc51491b5902146eea90a7f51f5f3021da
MathildeMalherbe <37385401+MathildeMalherbe@users.noreply.github.com>
2018-04-06 11:47:52 +0200

    Update Block.java

--- src/duckcoincoin/Block.java
+++ src/duckcoincoin/Block.java
@@ -1,5 +1,24 @@
 package duckcoincoin;
 
-public class Block {
+import java.util.Date;
 
+public class Block {
+	private int nonce;
+	private Transaction transactions[];
+	private String hash_block;
+	private String hash_prev;
+	private String hash_merkle; 
+	private Date timestamp;
+	
+	/**
+	 * Constructeur
+	 */
+	public Block (String hash_prev,Transaction transactions[],int difficulty){
+		this.hash_prev = hash_prev;
+		this.timestamp = timestamp;
+		merkle(transactions[]);
+		this.nonce = generateNonce(difficulty);
+		
+	}
 }
+
