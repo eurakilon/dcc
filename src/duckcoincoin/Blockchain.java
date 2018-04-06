0d8d96422502bdd92cb00e6a022b261ac0b97682
MathildeMalherbe <37385401+MathildeMalherbe@users.noreply.github.com>
2018-04-06 11:48:36 +0200

    Update Blockchain.java

--- src/duckcoincoin/Blockchain.java
+++ src/duckcoincoin/Blockchain.java
@@ -1,5 +1,19 @@
 package duckcoincoin;
 
 public class Blockchain {
+	private int difficulty;
+	private int nb_transactions_max;
+	private int nb_blocks;
+	private Block blocks[];
+	
+	public Blockchain(){
+		public void addBlock(Block blocks[]){
+			for (int i=0; i<nb_blocks;i++)
+				this.blocks[i] = new Block(this.blocks[i-1], this.generateTransaction(),this.difficulty);
+		}
+		public void checkBlockchain()
+		// etape 1 : vérifier le chainage des hashs
+		// etape 2 : vérifier tous les arbres de markle
+	}
 
 }
